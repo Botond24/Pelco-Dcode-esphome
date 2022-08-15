b14d499f71204a29b1bfe2287e07374616b5b2c7
Botond24 <44529287+Botond24@users.noreply.github.com>
2022-08-15 20:07:53 +0200

    Update __init__.py

--- components/pelco_dcode/__init__.py
+++ components/pelco_dcode/__init__.py
@@ -6,12 +6,12 @@ from esphome.const import CONF_ID, CONF_UART_ID
 DEPENDENCIES = ["uart"]
 ADDRESS = "address"
 
-PelcoDcode = cg.register_component("pelco_dcode", cg.Component)
+pelco_dcode = cg.register_component("pelco_dcode", cg.Component)
 
 CONFIG_SCHEMA = (
 	cv.Schema(
 		{
-			cv.GenerateID(): cv.declare_id(PelcoDcode),
+			cv.GenerateID(): cv.declare_id(pelco_dcode),
 			cv.Required(CONF_UART_ID): cv.use_id(uart.UARTDevice),
 			cv.Optional(ADDRESS, default=1): cv.int_range(1, 255),
 		}
